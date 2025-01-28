b088063fbd9a314220f3c96d057a1bfb22c102a0
salianashruth <salianashruth@gmail.com>
2025-01-29 00:43:16 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,5 +1,5 @@
 def call(String Project,String ImageTag, String raveruser){
-  withCredentials([usernamePassword('credentialsId':"raver666cred",passwordVariable:"raverpass",usernameVariable:"raveruser")])
+  withCredentials([usernamePassword('credentialsId':"raver666cred",passwordVariable:"raverpass",usernameVariable:"raveruser")]){
   sh "docker login -u ${raveruser} -p ${raverpass}"
   }
   sh "docker push ${raveruser}/${Project}:${ImageTag}"
