367cf85fb1d9d12e4caf77db41ba062d9ee5a11f
salianashruth <salianashruth@gmail.com>
2025-01-29 00:24:46 +0530

    Create dockerpush.groovy

---
+++ vars/dockerpush.groovy
@@ -0,0 +1,6 @@
+def call(String Project,String ImageTag, String raveruser){
+  withCredentials([usernamePassword('credentialsId':"raver666cred",passwordVariable:"raverpass",usernameVariable:"raveruser")])
+  sh "docker login -u ${raveruser} -p ${raverpass}"
+  }
+  sh "docker push ${raveruser}/${Project}:${ImageTag}"
+}
